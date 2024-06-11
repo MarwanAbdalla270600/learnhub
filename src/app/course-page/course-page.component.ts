d703000ef49f9831907b444883aac1895a343e76
Marwan Abdalla <marwan3m2000@gmail.com>
2024-06-11 17:06:34 +0200

    refactored course component

--- src/app/course-page/course-page.component.ts
+++ src/app/course-page/course-page.component.ts
@@ -3,7 +3,7 @@ import { CourseService } from '../general/services/course.service';
 import { CommonModule } from '@angular/common';
 import { CourseCardComponent } from '../general/components/course-card/course-card.component';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { combineLatest, map, startWith, switchMap } from 'rxjs';
+import { Observable, combineLatest, map, startWith, switchMap } from 'rxjs';
 import { Course, CourseSortValues, } from '../general/models/course-card';
 import { DropdownModule } from 'primeng/dropdown';
 import { ButtonModule } from 'primeng/button';
@@ -16,33 +16,32 @@ import { InputTextModule } from 'primeng/inputtext';
   templateUrl: './course-page.component.html',
   styleUrl: './course-page.component.css'
 })
-export class CoursePageComponent implements OnInit {
+export class CoursePageComponent {
 
+  //Services
   private readonly courseService = inject(CourseService);
   private readonly fb = inject(FormBuilder);
 
-  public readonly courses$ = this.courseService.getAllCourses();
-  public filteredCourses$ = this.courses$;
-  public sortValues: CourseSortValues[] = [CourseSortValues.Title, CourseSortValues.Price, CourseSortValues.Duration];
+  //Variables
+  sortValues: CourseSortValues[] = [CourseSortValues.Title, CourseSortValues.Price, CourseSortValues.Duration];
 
   searchForm: FormGroup = this.fb.group({
     name: [''],
     sort: [this.sortValues[0]]
   })
 
-  ngOnInit(): void {
-
-    const name$ = this.searchForm.get('name')!.valueChanges.pipe(startWith(''));
-    const sort$ = this.searchForm.get('sort')!.valueChanges.pipe(startWith(''));;
-
-    this.filteredCourses$ = combineLatest([name$, sort$]).pipe(
-      switchMap(([name, sort]) =>
-        this.courses$.pipe(
-          map(courses => this.filterAndSortCourses(courses, name, sort))
+  filteredCourses$: Observable<Course[]> =
+    combineLatest([
+      this.searchForm.get('name')!.valueChanges.pipe(startWith('')),
+      this.searchForm.get('sort')!.valueChanges.pipe(startWith(''))])
+      .pipe(
+        switchMap(([name, sort]) =>
+          this.courseService.getAllCourses().pipe(
+            map(courses => this.filterAndSortCourses(courses, name, sort))
+          )
         )
-      )
-    );
-  }
+      );
+
 
   private compareCourses(a: Course, b: Course, sort: CourseSortValues): number {
     switch (sort) {
@@ -62,7 +61,5 @@ export class CoursePageComponent implements OnInit {
       .filter(course => course.title.toLowerCase().includes(name.toLowerCase()))
       .sort((a, b) => this.compareCourses(a, b, sort));
   }
-
-
 
 }
